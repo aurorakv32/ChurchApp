ac35c038063d9a1bdb920d706834b04e78bf16f3
Aurora Wilson <Aurora Wilson>
2017-02-14 12:57:53 -0500

    added groups

--- modules/volunteers/server/models/volunteer.server.model.js
+++ modules/volunteers/server/models/volunteer.server.model.js
@@ -13,13 +13,7 @@ var VolunteerSchema = new Schema({
   title: {
     type: String,
     default: '',
-    required: 'Please enter a title',
-    trim: true
-  },
-  description: {
-    type: String,
-    default: '',
-    required: 'Please enter a description of your volunteer event',
+    required: 'Please enter an event name',
     trim: true
   },
   eventLeader: {
@@ -28,7 +22,29 @@ var VolunteerSchema = new Schema({
     required: 'Please enter the event leader name',
     trim: true
   },
-  
+  description: {
+    type: String,
+    default: '',
+    required: 'Please enter a description of your volunteer event',
+    trim: true
+  },
+  location: {
+    type: String,
+    default: 'Rock Point Church',
+    trim: true
+  },  
+  when: {
+    type: Date
+  },  
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+    //Tie in user to update correctly on view-volunteer.client.view.html
+  }
 });
 
 mongoose.model('Volunteer', VolunteerSchema);
